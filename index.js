8cd6d5ba8d5fa3c3ed1e711aa65187e7819fa77b
itsyashc <itsyashchourasiya@gmail.com>
2022-04-18 17:54:57 +0530

    Final

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ document.addEventListener("keydown", function(event){
 
 function makeSound(key){
     switch (key){
-        case "u":
+        case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
